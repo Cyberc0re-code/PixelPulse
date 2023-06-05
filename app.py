5680aa53bcfa095a2143ea57f97fe77dfd7f02d7
Cyberc0re <75711664+Cyberc0re-code@users.noreply.github.com>
2023-06-05 15:30:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,58 @@
+# pylint: disable=wildcard-import, unused-wildcard-import, wrong-import-position.
+
+"""
+@License: MIT
+"""
+
+import os
+from flask import Flask, request, redirect, url_for, flash, session
+
+import firebase_admin
+from firebase_admin import credentials, auth
+
+cred = credentials.Certificate("credentials.json")
+firebase_admin.initialize_app(
+    cred,
+    {"databaseURL": os.environ["FIREBASE_DB_URL"]},
+)
+
+app = Flask(__name__, static_url_path="/static", static_folder="static")
+
+app.config["DEBUG"] = False
+app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
+
+
+@app.before_request
+def validate_session():
+    """Validate each request with session cookie and firebase auth"""
+    # check if user is visiting login page
+    if request.path == "/login" or request.path == "/track":
+        return None
+
+    # read session cookie
+    session_cookie = request.cookies.get("secure-session")
+    if not session_cookie:
+        # Session cookie is unavailable. Force user to login.
+        return redirect(url_for("login"), 303)
+
+    try:
+        decoded_claims = auth.verify_session_cookie(session_cookie, check_revoked=True)
+        session["uid"] = decoded_claims["uid"]
+        return None
+    except auth.ExpiredSessionCookieError:
+        # Session cookie has been revoked. Force user to login.
+        flash("Session expired. Please login again.")
+        return redirect(url_for("login"))
+    except auth.InvalidSessionCookieError:
+        # Session cookie is invalid, expired or revoked. Force user to login.
+        flash("Please login to continue...")
+        return redirect(url_for("login"), 303)
+
+
+# import routes after app initialized
+from routes import *
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
+
